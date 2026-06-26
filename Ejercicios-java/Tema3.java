d2fa8ee19526ef6fa1e66e3a7fe6b379dc664a50
arekushinifa <arekushinifa@gmail.com>
2026-06-26 21:28:34 +0200

    Aqui usamos los switch

---
+++ Ejercicios-java/Tema3.java
@@ -0,0 +1,36 @@
+public class Tema3 {
+    public static void main(String[] args) {
+        
+        String dia = "";
+        int diasemana = 1;
+        
+        switch (diasemana) {
+            case 1:
+                dia = "Lunes";
+                break;
+            case 2:
+                dia = "Martes";
+                break;
+            case 3:
+                dia = "Miércoles";
+                break;
+            case 4:
+                dia = "Jueves";
+                break;
+            case 5:
+                dia = "Viernes";
+                break;
+            case 6:
+                dia = "Sábado";
+                break;
+            case 7:
+                dia = "Domingo";
+                break;
+            default:
+                dia = "?";
+
+        }
+        
+        System.out.println(dia);
+    }
+}
